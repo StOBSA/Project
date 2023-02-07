a4ae1cd653dc1356a1c06e588ccc28971cab107f
Plastikpizza <l.hassel@student.vu.nl>
2023-02-07 11:04:48 +0100

    reverting to HashSets to avoid cross-cmpl. errors

--- src/corners.rs
+++ src/corners.rs
@@ -1,5 +1,4 @@
 use indexmap::IndexSet;
-use itertools::Itertools;
 
 #[derive(Clone)]
 pub struct Corners {
@@ -36,74 +35,80 @@ impl FromIterator<usize> for Corners {
     }
 }
 
-#[derive(Clone)]
-pub struct BinaryCorners {
-    pub included : rug::Integer
-}
-
-impl BinaryCorners {
-    pub fn new() -> BinaryCorners {
-        BinaryCorners{included:rug::Integer::ZERO}
+use itertools::Itertools;
+impl std::fmt::Debug for Corners {
+        fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+            f.write_str(format!("{:?}", self.included.iter().collect_vec()).as_str())
+        }
     }
+// #[derive(Clone)]
+// pub struct BinaryCorners {
+//     pub included : rug::Integer
+// }
+
+// impl BinaryCorners {
+//     pub fn new() -> BinaryCorners {
+//         BinaryCorners{included:rug::Integer::ZERO}
+//     }
     
-    pub fn iter(&self) -> impl Iterator<Item=usize> + Clone + '_ {
-        use rug::Complete;
-        BinaryCornersIterator{
-            corners:self.clone(),
-            index:0,
-            mask :rug::Integer::parse("1").unwrap().complete()
-        }
-    }
+//     pub fn iter(&self) -> impl Iterator<Item=usize> + Clone + '_ {
+//         use rug::Complete;
+//         BinaryCornersIterator{
+//             corners:self.clone(),
+//             index:0,
+//             mask :rug::Integer::parse("1").unwrap().complete()
+//         }
+//     }
 
-    pub fn insert(&mut self, n : usize) {
-        self.included.set_bit(n as u32, true);
-    }
+//     pub fn insert(&mut self, n : usize) {
+//         self.included.set_bit(n as u32, true);
+//     }
 
-    pub fn remove(&mut self, n : &usize) {
-        self.included.set_bit(*n as u32, false);
-    }
+//     pub fn remove(&mut self, n : &usize) {
+//         self.included.set_bit(*n as u32, false);
+//     }
 
-    pub fn contains(&self, n : &usize) -> bool{
-        self.included.get_bit(*n as u32)
-    }
-}
+//     pub fn contains(&self, n : &usize) -> bool{
+//         self.included.get_bit(*n as u32)
+//     }
+// }
 
-#[derive(Clone)]
-struct BinaryCornersIterator {
-    mask : rug::Integer, 
-    index : usize,
-    corners : BinaryCorners
-}
+// #[derive(Clone)]
+// struct BinaryCornersIterator {
+//     mask : rug::Integer, 
+//     index : usize,
+//     corners : BinaryCorners
+// }
 
-impl Iterator for BinaryCornersIterator {
-    type Item = usize;
+// impl Iterator for BinaryCornersIterator {
+//     type Item = usize;
 
-    fn next(&mut self) -> Option<Self::Item> {
-        loop {
-            let result = self.corners.included.get_bit(self.index as u32);
-            self.index += 1;
-            self.mask <<= 1;
-            if result {
-                return Some(self.index-1)
-            } else if self.mask >= self.corners.included {
-                return None
-            }
-        }
-    }
-}
+//     fn next(&mut self) -> Option<Self::Item> {
+//         loop {
+//             let result = self.corners.included.get_bit(self.index as u32);
+//             self.index += 1;
+//             self.mask <<= 1;
+//             if result {
+//                 return Some(self.index-1)
+//             } else if self.mask >= self.corners.included {
+//                 return None
+//             }
+//         }
+//     }
+// }
 
-impl FromIterator<usize> for BinaryCorners {
-    fn from_iter<T: IntoIterator<Item = usize>>(iter: T) -> Self {
-        let mut corners = BinaryCorners::new();
-        for i in iter {
-            corners.insert(i);
-        }
-        corners
-    }
-}
+// impl FromIterator<usize> for BinaryCorners {
+//     fn from_iter<T: IntoIterator<Item = usize>>(iter: T) -> Self {
+//         let mut corners = BinaryCorners::new();
+//         for i in iter {
+//             corners.insert(i);
+//         }
+//         corners
+//     }
+// }
 
-impl std::fmt::Debug for BinaryCorners {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        f.write_str(format!("{:?}", self.iter().collect_vec()).as_str())
-    }
-}
+// impl std::fmt::Debug for BinaryCorners {
+//     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+//         f.write_str(format!("{:?}", self.iter().collect_vec()).as_str())
+//     }
+// }
