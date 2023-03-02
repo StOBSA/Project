b69a2f232896fafe07b3d7a6dc5d7a67d942393b
Plastikpizza <l.hassel@student.vu.nl>
2023-03-02 14:30:50 +0100

    removing compiler warnings

--- src/util.rs
+++ src/util.rs
@@ -12,16 +12,16 @@ pub fn to_point(point: OPoint) -> Point {
     (*point.0, *point.1)
 }
 
-pub fn is_improvement_by_factor(current_value : f32, new_value : f32, factor : f32) -> bool {
-    new_value < (current_value-current_value*factor)
-}
+// pub fn is_improvement_by_factor(current_value : f32, new_value : f32, factor : f32) -> bool {
+//     new_value < (current_value-current_value*factor)
+// }
 
-pub fn average_from_iterator<I:Iterator<Item=f32> + Clone>(values : I) -> f32 {
-    let mut len = 0;
-    let mut sum = 0.0;
-    for number in values {
-        sum += number * 1000.0;
-        len += 1;
-    }
-    sum / (len * 1000) as f32
-}
+// pub fn average_from_iterator<I:Iterator<Item=f32> + Clone>(values : I) -> f32 {
+//     let mut len = 0;
+//     let mut sum = 0.0;
+//     for number in values {
+//         sum += number * 1000.0;
+//         len += 1;
+//     }
+//     sum / (len * 1000) as f32
+// }
